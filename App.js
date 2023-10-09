366fcc2fd6bd71f7ec73fd219c888e1d40920057
StrukovSergei <strukov.sergei.999@gmail.com>
2023-10-09 11:38:18 +0300

    SearchForm cmp

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import SearchForm from './src/components/SearchForm';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <SearchForm />
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
