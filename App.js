d471f0ef42e5548ddb9c096eec439453759068ce
StrukovSergei <strukov.sergei.999@gmail.com>
2023-10-11 10:21:54 +0300

    app finalized

--- App.js
+++ App.js
@@ -1,27 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
-
+import {
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+  Text,
+  View,
+} from 'react-native';
 import SearchForm from './src/components/SearchForm';
 import FlightOptionItem from './src/components/FlightOptionItem';
-
-import data from './data.json'
+import dummyData from './data.json';
+import { useState } from 'react';
+import { searchFlights } from './src/services/api';
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const onSearch = async (data) => {
-    console.log(data);
-  }
+    setLoading(true);
+    setItems([]);
+
+    // get items form the backend
+    const response = await searchFlights(data);
+
+    setItems(response.data);
+    setLoading(false);
+  };
 
   return (
     <View style={styles.container}>
       <SafeAreaView>
-        <SearchForm />
+        <SearchForm onSearch={onSearch} />
+
+        {loading && (
+          <View>
+            <ActivityIndicator />
+            <Text>Searching for the best prices...</Text>
+          </View>
+        )}
+
         <FlatList
-          data={data}
+          data={items}
           renderItem={({ item }) => <FlightOptionItem flight={item} />}
           showsVerticalScrollIndicator={false}
         />
       </SafeAreaView>
+
       <StatusBar style="auto" />
     </View>
   );
